e0222a47724ea519221834c5aef44a9f41a67df5
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-04-27 18:36:32 +0300

    2

--- js/slick.js
+++ js/slick.js
@@ -1,22 +1,5 @@
 $(function () {
-  $(".class-gallery-list").slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: false,
-    centerMode: true,
-    cssEase: "linear",
-    prevArrow: ".class-gallery-list-left",
-    nextArrow: ".class-gallery-list-right",
-  });
-});
-
-$(function () {
-  $(".class-reviews-list").slick({
+  $(".w-q-why-list").slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -27,7 +10,87 @@ $(function () {
     dots: true,
     centerMode: true,
     cssEase: "linear",
-    prevArrow: ".class-reviews-list-left",
-    nextArrow: ".class-reviews-list-right",
+    prevArrow: ".w-q-why-list-left",
+    nextArrow: ".w-q-why-list-right",
+  });
+});
+$(function () {
+  $(".w-q-uniqueness-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".w-q-uniqueness-list-left",
+    nextArrow: ".w-q-uniqueness-list-right",
+  });
+});
+$(function () {
+  $(".w-q-reviews-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".w-q-reviews-list-left",
+    nextArrow: ".w-q-reviews-list-right",
+  });
+});
+$(function () {
+  $(".w-q-how-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".w-q-how-list-left",
+    nextArrow: ".w-q-how-list-right",
+  });
+});
+$(function () {
+  $(".w-q-characters-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".w-q-characters-list-left",
+    nextArrow: ".w-q-characters-list-right",
+  });
+});
+$(function () {
+  $(".w-q-home-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".w-q-home-list-left",
+    nextArrow: ".w-q-home-list-right",
   });
 });
